e8776dfbd1346bc8e70420f4b98d23438bcec4ce
ZHU YUE <charleshenryhugo@gmail.com>
2019-11-05 19:21:53 +0900

    break methods into smaller pieces ( customer_main_menu, admin_main_menu)

--- lib/screens/admin_main_menu.dart
+++ lib/screens/admin_main_menu.dart
@@ -12,53 +12,61 @@ class _AdminMainMenuState extends State<AdminMainMenu> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              child: Text(
-                currentSignedInUser.name[0],
-                style: TextStyle( fontSize: 40.0 ),
-              ),
+      children: <Widget>[
+        _buildAccountsDrawerHeader(context),
+        FlatButton(
+          onPressed: () {},
+          child: Text('Products'),
+        ),
+        FlatButton(
+          onPressed: () {},
+          child: Text('Settings'),
+        ),
+        Divider(),
+        _buildLogoutButton(context),
+      ],
+    );
+  }
+
+  Widget _buildLogoutButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric( horizontal: 16.0, ),
+      child: RaisedButton(
+        onPressed: () {
+          signOutGoogle();
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) {
+                return LoginPage();
+              }
             ),
-            accountName: Text(
-              currentSignedInUser.name,
-              style: TextStyle( fontSize: 20.0, fontWeight: FontWeight.w700),
-            ),
-            accountEmail: Text(
-              currentSignedInUser.email,
-              style: TextStyle( fontSize: 15.0 ) 
-            ),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-            ),
-          ),
-          FlatButton(
-            onPressed: () {},
-            child: Text('Products'),
-          ),
-          FlatButton(
-            onPressed: () {},
-            child: Text('Settings'),
-          ),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.symmetric( horizontal: 16.0, ),
-            child: RaisedButton(
-              onPressed: () {
-                signOutGoogle();
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    }
-                  ),
-                  ModalRoute.withName('/')
-                );
-              },
-              child: Text('Logout'),
-            ),
-          ),
-        ],
+            ModalRoute.withName('/')
+          );
+        },
+        child: Text('Logout'),
+      ),
+    );
+  }
+
+  Widget _buildAccountsDrawerHeader(BuildContext context) {
+    return UserAccountsDrawerHeader(
+      currentAccountPicture: CircleAvatar(
+        child: Text(
+          currentSignedInUser.name[0],
+          style: TextStyle( fontSize: 40.0 ),
+        ),
+      ),
+      accountName: Text(
+        currentSignedInUser.name,
+        style: TextStyle( fontSize: 20.0, fontWeight: FontWeight.w700),
+      ),
+      accountEmail: Text(
+        currentSignedInUser.email,
+        style: TextStyle( fontSize: 15.0 ) 
+      ),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+      ),
     );
   }
 }
--- lib/screens/customer_main_menu.dart
+++ lib/screens/customer_main_menu.dart
@@ -7,25 +7,7 @@ class CustomerMainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
         children: <Widget>[
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              child: Text(
-                currentSignedInUser.name[0],
-                style: TextStyle( fontSize: 40.0 ),
-              ),
-            ),
-            accountName: Text(
-              currentSignedInUser.name,
-              style: TextStyle( fontSize: 20.0, fontWeight: FontWeight.w700),
-            ),
-            accountEmail: Text(
-              currentSignedInUser.email,
-              style: TextStyle( fontSize: 15.0 ) 
-            ),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-            ),
-          ),
+          _buildAccountsDrawerHeader(context),
           FlatButton(
             onPressed: () {},
             child: Text('My Orders'),
@@ -35,25 +17,50 @@ class CustomerMainMenu extends StatelessWidget {
             child: Text('Settings'),
           ),
           Divider(),
-          Padding(
-            padding: const EdgeInsets.symmetric( horizontal: 16.0, ),
-            child: RaisedButton(
-              onPressed: () {
-                signOutGoogle();
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    }
-                  ),
-                  ModalRoute.withName('/')
-                );
-              },
-              child: Text('Logout'),
-            ),
-          ),
-
+          _buildLogoutButton(context),
         ],
       );
+  }
+
+  Widget _buildLogoutButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric( horizontal: 16.0, ),
+      child: RaisedButton(
+        onPressed: () {
+          signOutGoogle();
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) {
+                return LoginPage();
+              }
+            ),
+            ModalRoute.withName('/')
+          );
+        },
+        child: Text('Logout'),
+      ),
+    );
+  }
+
+  Widget _buildAccountsDrawerHeader(BuildContext context) {
+    return UserAccountsDrawerHeader(
+      currentAccountPicture: CircleAvatar(
+        child: Text(
+          currentSignedInUser.name[0],
+          style: TextStyle( fontSize: 40.0 ),
+        ),
+      ),
+      accountName: Text(
+        currentSignedInUser.name,
+        style: TextStyle( fontSize: 20.0, fontWeight: FontWeight.w700),
+      ),
+      accountEmail: Text(
+        currentSignedInUser.email,
+        style: TextStyle( fontSize: 15.0 ) 
+      ),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+      ),
+    );
   }
 }
