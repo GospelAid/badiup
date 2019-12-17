c5a9675e5ca7361add1f3b88eda67a3dd80d877d
dragonfly91 <samuel.anudeep@gmail.com>
2019-12-18 01:31:39 +0900

    Added linear progress indicator during login

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
-
-import 'package:badiup/sign_in.dart';
-import 'package:badiup/test_keys.dart';
+import 'package:badiup/colors.dart';
 import 'package:badiup/screens/admin_home_page.dart';
 import 'package:badiup/screens/customer_home_page.dart';
+import 'package:badiup/sign_in.dart';
+import 'package:badiup/test_keys.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,24 +11,28 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _loginInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: _buildBackgroundDecoration(context),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 150),
-              _buildLoginButton(),
-            ],
-          ),
-        ],
-      ),
+      body: _loginInProgress
+          ? LinearProgressIndicator()
+          : Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Container(
+                  decoration: _buildBackgroundDecoration(context),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 150),
+                    _buildLoginButton(context),
+                  ],
+                ),
+              ],
+            ),
     );
   }
 
@@ -41,49 +45,63 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _buildLoginButton() {
+  Widget _buildLoginButton(BuildContext context) {
     return RaisedButton(
       key: Key(makeTestKeyString(TKUsers.user, TKScreens.login, "loginButton")),
-      onPressed: () {
-        signInWithGoogle().whenComplete(() {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) {
-                return currentSignedInUser.isAdmin()
-                    ? AdminHomePage(title: 'BADI UP')
-                    : CustomerHomePage(title: 'BADI UP');
-              },
-            ),
-          );
-        });
-      },
-      color: Color(0xFF151515),
+      onPressed: () => _doLogin(context),
+      color: paletteBlackColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
       highlightElevation: 100,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Image(
-              image: AssetImage("assets/google_logo.png"),
-              height: 20,
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'Login with Google',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFD2D1D1),
-                ),
-              ),
-            )
-          ],
+      child: _buildLoginButtonInternal(),
+    );
+  }
+
+  void _doLogin(BuildContext context) {
+    setState(() {
+      _loginInProgress = true;
+    });
+    
+    signInWithGoogle().whenComplete(() {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return currentSignedInUser.isAdmin()
+                ? AdminHomePage(title: 'BADI UP')
+                : CustomerHomePage(title: 'BADI UP');
+          },
         ),
+      );
+    
+      setState(() {
+        _loginInProgress = false;
+      });
+    });
+  }
+
+  Widget _buildLoginButtonInternal() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image(
+            image: AssetImage("assets/google_logo.png"),
+            height: 20,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 15),
+            child: Text(
+              'Login with Google',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: paletteLightGreyColor,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
