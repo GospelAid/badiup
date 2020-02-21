d687083f1098e28f3edf04168188553d89d31769
ZHU YUE <charleshenryhugo@gmail.com>
2020-02-21 20:27:51 +0900

    add order item list

--- lib/screens/admin_order_detail_page.dart
+++ lib/screens/admin_order_detail_page.dart
@@ -1,8 +1,11 @@
+import 'package:badiup/models/product_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:intl/intl.dart';
+import 'package:transparent_image/transparent_image.dart';
 import 'package:badiup/colors.dart';
+import 'package:badiup/constants.dart' as constants;
 import 'package:badiup/models/order_model.dart';
 
 class AdminOrderDetailPage extends StatefulWidget {
@@ -15,8 +18,14 @@ class AdminOrderDetailPage extends StatefulWidget {
 }
 
 class _AdminOrderDetailPageState extends State<AdminOrderDetailPage> {
+  Map<String, OrderItem> orderItems = Map<String, OrderItem>();
+
   @override
   Widget build(BuildContext context) {
+    widget.order.items.forEach( (item) {
+      orderItems[item.productId] = item;
+    } );
+
     return Scaffold(
       appBar: _buildAppBar(context),
       body: _buildBody(context),
@@ -47,7 +56,7 @@ class _AdminOrderDetailPageState extends State<AdminOrderDetailPage> {
         children: <Widget>[
           _buildOrderPlacedDateText(),
           _buildOrderStatusDescriptionBar(),
-          _buildOrderItemList(),
+          _buildOrderItemBlocks(context),
           _buildOrderPriceDescriptionBar(),
         ],
       ),
@@ -97,19 +106,74 @@ class _AdminOrderDetailPageState extends State<AdminOrderDetailPage> {
     );
   }
 
-  Widget _buildOrderItemList() {
+  Widget _buildOrderItemBlocks(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance
+            .collection(constants.DBCollections.products)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return LinearProgressIndicator();
+          }
+
+          return _buildOrderItemList(context, snapshot.data.documents);
+        }
+      ),
+    ); 
+  }
+
+  Widget _buildOrderItemList(BuildContext context, List<DocumentSnapshot> snapshots) {
+    return Column(
+      children: snapshots.where(
+        (snapshot) => orderItems.keys.contains(snapshot.documentID)
+      ).map(
+        (snapshot) => _buildOrderItemListItem(context, Product.fromSnapshot(snapshot))
+      ).toList(),
+    );
+  }
+
+  Widget _buildOrderItemListItem(BuildContext context, Product product) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: const Color(0xFFA2A2A2)),
+        ),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
         children: <Widget>[
-          // Expanded(
-          //   child: ListView(
-          //     children: <Widget>[],
-          //   ),
-          // ),
+          _buildProductImage(product),
+          //Column(),
+
         ],
       ),
+    );
+  }
+
+  // TODO _buildProductImage() can become an utility function
+  Widget _buildProductImage(Product product) {
+    Widget productImage;
+    if (product.imageUrls?.isEmpty ?? true) {
+      productImage = Image.memory(
+        kTransparentImage,
+        height: 100,
+        width: 100,
+      );
+    } else {
+      productImage = FadeInImage.memoryNetwork(
+        fit: BoxFit.cover,
+        height: 100,
+        width: 100,
+        placeholder: kTransparentImage,
+        image: product.imageUrls.first,
+      );
+    }
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12.0),
+      child: productImage,
     );
   }
 
@@ -134,7 +198,7 @@ class _AdminOrderDetailPageState extends State<AdminOrderDetailPage> {
             ),
             Container(
               child: Text(
-                widget.order.getOrderPrice().toString(),
+                "¥${NumberFormat("#,##0").format(widget.order.getOrderPrice())}",
                 style: TextStyle(
                   color: paletteDarkRedColor,
                   fontSize: 16,
