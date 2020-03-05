f784c6ec2852ed43d82ba7e3e2a46ed5e1e6ca08
ZHU YUE <charleshenryhugo@gmail.com>
2020-03-05 18:53:55 +0900

    update admin_settings_listing page

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,6 +1,6 @@
 import 'package:badiup/colors.dart';
+import 'package:badiup/constants.dart' as constants;
 import 'package:badiup/sign_in.dart';
-import 'package:badiup/screens/admin_home_page.dart';
 import 'package:badiup/widgets/banner_button.dart';
 import 'package:badiup/widgets/admin_settings_listing.dart';
 import 'package:flutter/material.dart';
@@ -11,35 +11,34 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  Widget _buildSettingsListing(){
-    if ( currentSignedInUser.isAdmin()) return _buildAdminSettingsListing();
-    return _buildCustomerSettingsListing();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("設定",
-            style: TextStyle(
-              color: paletteBlackColor,
-              fontWeight: FontWeight.bold,
-              fontSize: 14,
-            )),
-        elevation: 0.0,
-        backgroundColor: paletteLightGreyColor,
-        iconTheme: IconThemeData(color: paletteBlackColor),
-        centerTitle: true,
-      ),
-      body: _buildSettingsListing(),
+      appBar: _buildAppBar(),
+      body: currentSignedInUser.isAdmin() ?
+        _buildAdminSettingsListing() : _buildCustomerSettingsListing(),
       bottomNavigationBar: BannerButton(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AdminHomePage()),
-          );
-        },
         text: "保存",
+        onTap: () async {
+          _saveCurrentSignedInUserData();
+          Navigator.pop( context );
+        },
       ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      title: Text("設定",
+          style: TextStyle(
+            color: paletteBlackColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          )),
+      elevation: 0.0,
+      backgroundColor: paletteLightGreyColor,
+      iconTheme: IconThemeData(color: paletteBlackColor),
+      centerTitle: true,
     );
   }
 
@@ -61,9 +60,21 @@ class _SettingsPageState extends State<SettingsPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Text('Customer Settings Listing'),
+          // TODO customer settings listing
+          Container(),
         ],
       ),
     );
+  }
+
+  Future<void> _saveCurrentSignedInUserData() async {
+    if (currentSignedInUser.isAdmin()) {
+      currentSignedInUser.setting.taxInclusive = taxInclusiveBoolValue;
+    }
+
+    await db
+        .collection(constants.DBCollections.users)
+        .document(currentSignedInUser.email)
+        .updateData(currentSignedInUser.toMap());
   }
 }
